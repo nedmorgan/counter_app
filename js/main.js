247fc34a4edaceeda3ea4a847a1b953bfa2adb53
nedmorgan <nmorgan1987@gmail.com>
2018-11-27 18:03:37 -0500

    added functionality to counter

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 let count = 0;
 const addition = document.getElementById('addition');
 const subtraction = document.getElementById('subtraction');
-let total = document.getElementById('total').innerHTML;
 
 const add = () => {
   if(count >= 0 || count <= 0) {
     count++;
     console.log(count);
+    document.querySelector('p').innerHTML = count;
   }
 };
 
@@ -14,10 +14,9 @@ const subtract = () => {
   if(count >= 0 || count <= 0) {
     count--;
     console.log(count);
+    document.querySelector('p').innerHTML = count;
   }
 };
 
 addition.addEventListener("click", add);
 subtraction.addEventListener("click", subtract);
-
-total = count;
